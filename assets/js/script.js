645bab53f05e6b852afd6c2e7fe19377a7269609
Ryely <ryely.moore@outlook.com>
2021-06-10 00:01:28 -0600

    added alerts for when a city can not be found or when the fetch fails

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ var weatherData = function(city) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ef42ec77e5abd2ef83947df102ff17d6&units=imperial")
     .then(function(res){
         res.json().then(function(data){
+            if(data.cod = 404) {
+                alert("Sorry the City you are looking for could not be found.");
+            }
             console.log(data);
         })
+    }).catch(function(reason){
+        alert(reason);
     })
 }
 
