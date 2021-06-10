49fd6b5a1c458d89382976ab562543261aaf76cd
Ryely <ryely.moore@outlook.com>
2021-06-10 00:51:52 -0600

    wanted to use open weather one call for the to get current and 7 day forecast so had to add geocoding api call, will get lon and lat data for requested city but will also check with the user what they meant if multiple cities are found.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,36 @@
 var weatherData = function(city) {
     console.log("start");
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ef42ec77e5abd2ef83947df102ff17d6&units=imperial")
-    .then(function(res){
-        res.json().then(function(data){
-            if(data.cod = 404) {
-                alert("Sorry the City you are looking for could not be found.");
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=ef42ec77e5abd2ef83947df102ff17d6")
+    .then(function(res) {
+        res.json().then(function(data) {
+            if (data.length > 1) {
+                for(var i = 0; i < data.length; i++){
+                    var conf = confirm("Is the city you are looking for " + data[i].name + ", " + data[i].country + ", " + data[i].state + "?");
+                    if (conf) {
+                        var coord = { "lon": data[i].lon, "lat": data[i].lat}
+                        return coord;
+                    }
+                }
+            } else {
+                var coord = { "lon": data[i].lon, "lat": data[i].lat}
+                        return coord;
             }
-            console.log(data);
+        }).then(function(coord){
+            console.log(coord);
         })
-    }).catch(function(reason){
-        alert(reason);
     })
+
+    // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ef42ec77e5abd2ef83947df102ff17d6&units=imperial")
+    // .then(function(res){
+    //     res.json().then(function(data){
+    //         if(data.cod = 404) {
+    //             alert("Sorry the City you are looking for could not be found.");
+    //         }
+    //         console.log(data);
+    //     })
+    // }).catch(function(reason){
+    //     alert(reason);
+    // })
 }
 
 $("#target").submit(function(event){
