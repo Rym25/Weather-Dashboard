fda55164fba68a2b737a98c2e1f2d7893b9db25e
Ryely <ryely.moore@outlook.com>
2021-06-10 01:39:20 -0600

    reworked geo call and weather call into 2 functions to fix issue where longitude and latitude not working being set correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,45 @@
-var weatherData = function(city) {
+var geoData = function(city) {
     console.log("start");
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=ef42ec77e5abd2ef83947df102ff17d6")
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=ef42ec77e5abd2ef83947df102ff17d6")
     .then(function(res) {
         res.json().then(function(data) {
+            console.log(data);
             if (data.length > 1) {
                 for(var i = 0; i < data.length; i++){
                     var conf = confirm("Is the city you are looking for " + data[i].name + ", " + data[i].country + ", " + data[i].state + "?");
                     if (conf) {
                         var coord = { "lon": data[i].lon, "lat": data[i].lat}
-                        return coord;
+                        weatherData(coord);
+                        break;
                     }
                 }
             } else {
                 var coord = { "lon": data[i].lon, "lat": data[i].lat}
-                        return coord;
+                        weatherData(coord);
             }
-        }).then(function(coord){
-            console.log(coord);
         })
     })
+}
 
-    // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ef42ec77e5abd2ef83947df102ff17d6&units=imperial")
-    // .then(function(res){
-    //     res.json().then(function(data){
-    //         if(data.cod = 404) {
-    //             alert("Sorry the City you are looking for could not be found.");
-    //         }
-    //         console.log(data);
-    //     })
-    // }).catch(function(reason){
-    //     alert(reason);
-    // })
+var weatherData = function(coord) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=ef42ec77e5abd2ef83947df102ff17d6")
+            .then(function(res){
+                res.json().then(function(data){
+                    displayWeather(data);
+                })
+            })
+}
+
+var displayWeather = function(data) {
+    
+    // displays the date of the weather from the daily forecast
+    var date = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
+    console.log(date);
+    console.log(data);
 }
 
 $("#target").submit(function(event){
     event.preventDefault();
     var city = $("#input-city").val();
-    weatherData(city);
+    geoData(city);
 })
