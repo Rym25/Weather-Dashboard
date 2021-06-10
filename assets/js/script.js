b85f456430dc63072a12859a0e530c9f8c0310a3
Ryely <ryely.moore@outlook.com>
2021-06-10 02:01:49 -0600

    added variables for data needed from the current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,19 +31,21 @@ var weatherData = function(coord) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=ef42ec77e5abd2ef83947df102ff17d6")
             .then(function(res){
                 res.json().then(function(data){
-                    displayWeather(data);
+                    displayCurrentWeather(data);
                 })
             }).catch(function(reason){
                 alert(reason);
             })
 }
 
-var displayWeather = function(data) {
-
-    // displays the date of the weather from the daily forecast
-    var date = moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
-    console.log(date);
-    console.log(data);
+var displayCurrentWeather = function(data) {
+    // gets the date of the weather from the current forecast
+    var date = moment.unix(data.current.dt).format("MM/DD/YYYY");
+    // gets the required weather data
+    var temp = data.current.temp;
+    var uvi = data.current.uvi;
+    var wind = data.current.wind_speed;
+    var humidity = data.current.humidity;
 }
 
 $("#target").submit(function(event){
